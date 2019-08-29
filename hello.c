85bc78935f40746ccdbae530f754c543854c8cdc
taylorrae27 <54685202+taylorrae27@users.noreply.github.com>
2019-08-29 14:48:13 -0500

    Fixed slash
    I used the forward slash instead of the back slash.

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Taylor Morgan
+ * Date: 2019/08/29
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+
+  return 0;
+}
